fdc70d724b1d6282ab851f79a93b6987868b4caa
andreanzi <a.anzilotti@hastega.it>
2023-08-03 13:56:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/andreanzi/stripe-kit.git", from: "17.1.0.1"),
+        .package(url: "https://github.com/andreanzi/stripe-kit.git", from: "17.1.1"),
     ],
     targets: [
         .target(name: "Stripe", dependencies: [
